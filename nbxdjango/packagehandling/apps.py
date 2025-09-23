37229e17e44625f6322525e6e5924faeb59b8ac2
Juan Almeida <juanelojga@gmail.com>
2025-09-23 14:21:36 -0500

    Add `Client` group creation, signal for user-group assignment, and update GraphQL query permissions
    - Add migration to create and remove `Client` group.
    - Introduce signal to automatically assign users to the `Client` group when `Client` is created.
    - Update GraphQL resolvers to include permission checks for `Package` and `Client` queries.

--- nbxdjango/packagehandling/apps.py
+++ nbxdjango/packagehandling/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PackagehandlingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'packagehandling'
+
+    def ready(self):
+        import packagehandling.signals
--- nbxdjango/packagehandling/schema.py
+++ nbxdjango/packagehandling/schema.py
@@ -1,6 +1,7 @@
 import graphene
 import graphql_jwt
 from django.contrib.auth import get_user_model
+from django.core.exceptions import PermissionDenied
 from graphene_django import DjangoObjectType
 from .models import Package, Client
 
@@ -25,23 +26,41 @@ class Query(graphene.ObjectType):
     client = graphene.Field(ClientType, id=graphene.Int())
     me = graphene.Field(UserType)
 
-    def resolve_all_packages(root, info):
-        return Package.objects.all()
-
-    def resolve_package(root, info, id):
-        return Package.objects.get(pk=id)
-
-    def resolve_all_clients(root, info):
-        return Client.objects.all()
-
-    def resolve_client(root, info, id):
-        return Client.objects.get(pk=id)
-
     def resolve_me(root, info):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Not authenticated')
+            return None
         return user
+
+    def resolve_all_clients(root, info):
+        if not info.context.user.is_superuser:
+            raise PermissionDenied()
+        return Client.objects.all()
+
+    def resolve_client(root, info, id):
+        user = info.context.user
+        if user.is_superuser:
+            return Client.objects.get(pk=id)
+        if not hasattr(user, 'client') or user.client.id != id:
+            raise PermissionDenied()
+        return Client.objects.get(pk=id)
+
+    def resolve_all_packages(root, info):
+        user = info.context.user
+        if user.is_superuser:
+            return Package.objects.all()
+        if hasattr(user, 'client'):
+            return Package.objects.filter(client=user.client)
+        return Package.objects.none()
+
+    def resolve_package(root, info, id):
+        user = info.context.user
+        package = Package.objects.get(pk=id)
+        if user.is_superuser:
+            return package
+        if not hasattr(user, 'client') or package.client != user.client:
+            raise PermissionDenied()
+        return package
 
 class Mutation(graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
