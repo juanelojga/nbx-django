e1fa3c1a719428b43d6f24f0666b3dea1efae760
Juan Almeida <juanelojga@gmail.com>
2025-10-04 18:16:41 -0500

    Add production-ready deployment configuration (#7)
    * Add production-ready setup and deployment configuration
    
    - Set up `Procfile` for deployment process.
    - Replace manual database configuration with `dj-database-url` for easier management.
    - Introduce `gunicorn` and `whitenoise` for production readiness.
    - Update CI workflow to include test coverage reporting and deploy step.
    - Enhance `.env.example` with `DATABASE_URL` for unified DB configuration.
    
    * Add pytest configuration to `pyproject.toml`
    
    - Configure `DJANGO_SETTINGS_MODULE` and `django_project_root` options for pytest-django integration.
    
    * Update pytest configuration and CI workflow
    
    - Remove unused `django_project_root` in `pyproject.toml` pytest settings.
    - Set working directory for pytest in CI workflow.
    - Simplify test command by removing explicit `DJANGO_SETTINGS_MODULE` declaration.
    
    * Update CI workflow to adjust coverage file path
    
    - Change `files` path for Codecov action to `./nbxdjango/coverage.xml`.
    
    * Make Codecov error handling conditional on the main branch

--- nbxdjango/nbxdjango/settings.py
+++ nbxdjango/nbxdjango/settings.py
@@ -14,6 +14,8 @@ import datetime
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dummy-ci-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS: list[str] = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "graphene_django",
     "graphql_jwt",
@@ -51,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -84,14 +88,11 @@ WSGI_APPLICATION = "nbxdjango.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "mydatabase"),
-        "USER": os.environ.get("DB_USER", "myuser"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "mypassword"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        default=os.environ.get("DATABASE_URL"),
+    )
 }
 
 
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
